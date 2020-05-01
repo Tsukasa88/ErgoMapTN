759f88af141f74e635a5fc36e9fbcc453738fdc1
Tsukasa88 <36668874+Tsukasa88@users.noreply.github.com>
2020-05-01 22:43:19 +0200

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,180 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "600", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TUN101: {
+      name: "Tozeur",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520092402823/?__cft__[0]=AZWw4jW-vWTCfK4s2lMsChiznNqWi_nIhFMllNFxI2zAv9OIqcqwiSiKcbPo_Qc1OucO2lb_RyPu8ImhKzxEYbZqceFlMY8yydGXUZ2ST7ALGjcnEIjtlr5bxokjySaRkx2GKJCqjne52jRr4oVcW9COV4kIjEUim9DZqlsOXu_Iiw&__tn__=%2CO%2CP-R"
+    },
+    TUN102: {
+      name: "Manouba",
+      description: "نجلاء الكامل السيد | نسرين سياري | فاطمة الحرشاني الجباري.",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913809112373921/?__cft__[0]=AZUl7lqtDCGK-YoyoD_FnfL7VIfLHy-99D5eQ2yyYVIi1V75jnC0ZFyJ00OiQ1e1gZsJcrmb0RChytmh4OkjS9Qhueb9GLMt42JWrFk4KAN8kQ&__tn__=%2CO%2CP-R"
+    },
+    TUN103: {
+      name: "Béja",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520009069498/?__cft__[0]=AZW_A5jTGOFx5gsNmdA520okcSlgyVdTQ7fuLJUvNtoPjHqvNwm2182PaMhIMqnP7C7QN3KWa9S34twn0frUghXOwszYOP3sfc6XBNCjiSbPU6AgRRue5Pnr5ghosjwYRpXrV1VIKulNQ6StkeBiz-Tvmc86fIAOTuyxivMvnhrfUw&__tn__=%2CO%2CP-R"
+    },
+    TUN104: {
+      name: "Ben Arous (Tunis Sud)",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913522795735886/?__cft__[0]=AZXJdVT6knChSmOsztZbX3tw9IQ7VTw1YFPrcyk0d9jtv2KI4vH70XH2PI8QRm5Agfe1hpofvMbaO_j9dVGFMIgStW5tA2IkXX18-Oc7fH7KCleFYpz_MoEIfL8e-qO3v69rP0SnRyakgbQ72aTqVFwLPrGNJgBxl7_rzhnswcN1jg&__tn__=%2CO%2CP-R"
+    },
+    TUN105: {
+      name: "Bizerte",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913521442402688/?__cft__[0]=AZV0Kjpdk6n5_dyVW9ElSnqOZ6HwlkllI5R3vDiZCo44qZxGa3_fhC957oN9TBuIblnLVGVnk4hMiNo4ko9kwZEUpiETEiPC6rJBVAS3RPq4Ee3ZLCysJGC_LKDGTaHv3Q3KbNi5SDxafnY7Hr2uoeo98DPSjBB7ZUMK2YOzuU4unQ&__tn__=%2CO%2CP-R"
+    },
+    TUN106: {
+      name: "Jendouba",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913521282402704/?__cft__[0]=AZWo3Ep0k1rKjojna8AMdF5A6UOQHYE4MDqJJhjyh7cI_CCgeTUysFcpEXkjysJWj3ZhX8_Cduz0GZMbFawFfNiVwIsaoILfdmjA8AjLbLXc_tLJZrNALKdx0F3ImcicrmWM1I0XUJhoIlCawksowc6cLAzNRUALjyLDTA4Fs4nbSg&__tn__=%2CO%2CP-R"
+    },
+    TUN107: {
+      name: "Nabeul",
+      description: "Zouhour Trabelsi | Mouna Zgoulli Kordoghli",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913524205735745/?__cft__[0]=AZVwC4zWEFmjlSftk3fzIsSkhQLu1-GY0Hjxsk9n_ogbByMfq3U6Zw6IJAgS4XC6a_vE5A7b12NxHYH09vDoaUupDLYgP0QTzrnjzWsX23MqjNOL-HqzbgXO7PPMquuxqNVZHSy1PFwOAKEarjpYADC4HetRJw0aGAmGg-UF3XHObw&__tn__=%2CO%2CP-R"
+    },
+    TUN108: {
+      name: "Tunis",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913521349069364/?__cft__[0]=AZUuRhxUQnge5h4wosCk_6u7lRMwKdSqBWJqfVRCsuOwJQSSFzNdccldXP-78jJ-iLIIx6uqWb9wpO7y6FbDB6JhuCWBHu2UwN0Ns-gejZ25x1hEJFC5Nl8yUcEhfl26m29Y3GEJAORPK5OaPlr2hXH8PvD0lNv1XgsBvWBTLAlESQ&__tn__=%2CO%2CP-R"
+    },
+    TUN109: {
+      name: "Le Kef",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520595736106/?__cft__[0]=AZWSIP5202ck_FC4UNzNESBWSapP2iWC9D-ISW19GcznJglU4vugPYetqt3urP6wqkqbAUord0mmWjeNkKNwf9mqbxAsshbW4bVdbGJzLKh5cK91Fmg5gL13G6g20EIqt0_5qFcsNH6_BOjatcml8h333RPfZ6vaqNJR0l0eVv9aWQ&__tn__=%2CO%2CP-R",
+      description: "حنان الجباري  |"
+    },
+    TUN110: {
+      name: "Kassérine",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520725736093/?__cft__[0]=AZUF3VtioB4UIpBK7Z3WhVfGcFueGhrx5lXK6czA9H6GEdj2jptMfTb6vXI51FYKKWwOY3nIAnap6n59AedzoJpnJj2zNvUPu_1Yf0jc4n4b8LSnkninriJz0GoM8Rxuw8uXXB1HuIYxcGi2W5LEP7cPs5FLIozvXSwhZmNAzr2luQ&__tn__=%2CO%2CP-R"
+    },
+    TUN111: {
+      name: "Gabès",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520685736097/?__cft__[0]=AZXHAFRG5ObUte-BXk8_ws9qMn44Qhak_BUGPpa5E5FuIpB9SN-AvykpBnLUwG1Y6JUwhbfCC1Afcoa9Zb3QvCpaepWUSAHdJMxekKd0sS5PjIknL55Lv4GYaS3KR3WfRMsybuZVKJ2HrmrksJCsG5MUzjPSbkM-vmjUWwqwxa55tw&__tn__=%2CO%2CP-R"
+    },
+    TUN112: {
+      name: "Gafsa",
+      description: "تونس ركروكي |  هاجرة مليكي",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913931635695002/?__cft__[0]=AZVfwkm8Gh_kBDy1A9KuxsVxsjVr-LYSxzyl0k9TDzLXXajmpyvLGcsgJ11oFuOQsD403z5ieyiBpGh4gAEcJadn5lf366pV2VC9akvkmDmrRUyfIN2O5fBMlLJJXIMqt-ABle8Ipcr7XObmsiDjFpo5&__tn__=%2CO%2CP-R"
+    },
+    TUN113: {
+      name: "Sidi Bou Zid",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913521212402711/?__cft__[0]=AZUUHbD3AyyxsoAh6xZlcGvrNPppk09m0zCiCU9ntoQEKnDgglSkvyfV8LdbWGrj6icvRrnC3bHyAS8QfW1SflSsDjjw1p8p3j3E1PwEmJTsviV3VOeb6DFEx0ui-l9G97BmhqEvgojXwBT_jIcv4wlXryY_7BKtSMQ34urZB8Ta7w&__tn__=%2CO%2CP-R"
+    },
+    TUN114: {
+      name: "Sfax",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520945736071/?__cft__[0]=AZX__vXvCcOg38uPUa1ZH3XP7rvPUHcTwC7NnDzpMspyoGrMLOZj_taOGgo84qJf1hNBz_TBprPjOWHnuipMxgWhBSG_lP2OwmAc1yIlqC4_iv3d0r61IKKMjebxQM7tXFc_5nIqcaqezvGpeIeFwkf0aXzQaoZHLyoNLKbaTTUynA&__tn__=%2CO%2CP-R",
+      description: "Boubakri Olfa | Naziha Siala"
+    },
+    TUN115: {
+      name: "Siliana",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520182402814/?__cft__[0]=AZWLxmb-R_AW4XYCw7wID9S93n_6gC_H2WV4NAvkqG5KrmaELO0wpua0HFmwGigddpb4xyx0whvyZ3CCtcjqDwQxQP3kL6xxADaMs72QC1w02IIu4mBtGcc7D1x0xVqer2e6eHOExVNA3UWvBXv7XZ5clyDAwb_xMmD2h8z_OP0ELg&__tn__=%2CO%2CP-R"
+    },
+    TUN116: {
+      name: "Mahdia",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913522709069228/?__cft__[0]=AZUDG5ABySUw-f5DGt6lV5fYTTdN5KY1z8kE3kTXazT6oa9HK6S8-qFGgAwDL-GPmDo1RC5eCRT3xmhKQTVtBKjICkF6LgRk2joaG36y2pF-z6mDuco0NZ4Ef_2QKc94UojdpiMJmvVrEE94yMHZTyYPFoyzSz0hws6eoNxfB9AEBw&__tn__=%2CO%2CP-R",
+      description: "فهمي الغراد"
+    },
+    TUN117: {
+      name: "Monastir",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520885736077/?__cft__[0]=AZU2fI6GsebMPWalwxp19vj0lenG7RCQ1a30-2l6PAL36PeP54_sWLgTB7NJ5Z-pYkpl27QyM8fpq2Cg-gZVo2Ri2GLvwE-ETckRnrxEy55bFb7CHOa99nLJjYqj3ozFiKYRFd0liyCPurgevS1jwTjPgHhIl4g8KXX30OXWK0jAEA&__tn__=%2CO%2CP-R"
+    },
+    TUN118: {
+      name: "Kairouan",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913521032402729/?__cft__[0]=AZWK6kxPLzCtvcYKGsF90gubjzaKjc1BZKxCI9UhDqQdE8ehOBylzFCKst5GjvWmaUHDTGVeYz3pkZ-3W7LKuT0OnKWeZW1q9RJ2tTvuUO1HXwHTIsB_rt2_qkN7bBvBOTsyafiO9njpN8O1mbauQ2NrAZPUmbL9zko6WDOM7YkpzA&__tn__=%2CO%2CP-R"
+    },
+    TUN119: {
+      name: "Sousse",
+      description: "Sameh Wallani | Marwa Omran Abid",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913522532402579/?__cft__[0]=AZU8LSCsWup7rDa87TK5u9kVGbszfYJyouZ7Tc2hSpAlG7BWinj0G53kOWvueW74ZFjv5mW2K3oeJFsRr0ROWbCQvqczzsiYe7GVS3FWYvBTASptGbwbXKuWbX_jQzvkcf5id2vvgA84QyisoUNSnPlBckydQ9d871046Qj7hjmxgA&__tn__=%2CO%2CP-R"
+    },
+    TUN120: {
+      name: "Zaghouan",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520299069469/?__cft__[0]=AZX_fQw0GRInduvkiu_s2_Yzv4nOWbJjGNMOtlz7JkzAAiCVrCFhg08odnvBxQxymYdOM10h-41R9Ta40sqAAcPeD2AxaPspku7vN14LBAsyueTP-QCKGgeqAkCFeIU8GT5s28qcxUgbKJgKv6tnFOizDQko6_X5mwUIMVvv3ftG-w&__tn__=%2CO%2CP-R"
+    },
+    TUN96: {
+      name: "Médenine",
+      description: "OumaYma ChniTer",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913524459069053/?__cft__[0]=AZV8lxALdJbIw4Leqrc_DSaGhCFQ-GT0B7lvPR0ozDPGzeJl2TwRFgjVb7FFunjza5p6JFDZspb0-ID_K4jMDSYfPG-T_eUMqNRcaME1uZM4r_cvE9n3KVq7VmXjy-bby6AqcY_6-oxLrMrF7DOuucQkFjGpe8hB595YNnEHCA8u6w&__tn__=%2CO%2CP-R"
+    },
+    TUN97: {
+      name: "Kebili",
+      description: "Ahlem Ben Mohamed",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913520532402779/?__cft__[0]=AZXooNoDiPydaR-J-NmgWvV4qgQrQy4x7LktW6VO4DDq1MpL_UU0Ko_Xd9LBOZNZwgplAmLVIyApuY6j4-RMnuZFhY9yH43lRcnsPWg3izFdY7uJYkYNy8rKbwebwxor07IFDt62RB9cSl4zsZfBpKyxyvZ0muvRSijz8wEmqsLi_Q&__tn__=%2CO%2CP-R"
+    },
+    TUN98: {
+      name: "Tataouine",
+      url: "https://www.facebook.com/groups/911604952594337/permalink/913521689069330/?__cft__[0]=AZWYrELm35WE0gnrJSd7RVCsCXmN7ZDj4blZ8LVp09Z3S0ZhIfbADZdfUYczl7WGhV_3JFpjXsSwAfFfwyW1LxvJPOCVhCQ2Pl2Pz0WA8IYMmAtLkBcGym4_1dt8s0R0jQCrznv0xg3I9Fpc6U2I8s9DdmsublnEPX4AvQxSbzk1bQ&__tn__=%2CO%2CP-R"
+    }
+  },
+  locations: {},
+  labels: {},
+  regions: {}
+};
